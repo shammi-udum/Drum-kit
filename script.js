e8f8a33e6934aaa0fcddcabba620fa958bb275ab
shammi-udum <shammi591@yahoo.com>
2024-03-11 00:42:16 -0400

    sound connection

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 window.addEventListener('keydown' ,function(e){
-console.log(e.keyCode)
+const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`)
+if(!audio) return; // stops the function
+audio.play();
 });
